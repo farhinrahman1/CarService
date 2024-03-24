91b175b5b4e8b130ac77593eea0c3d5ac8bb7e95
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-03-25 01:06:00 +0600

    thankyou page added

---
+++ thankyou.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thankyou</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include('partials/navbar.php'); ?>
+    <div class="container-thankyou">
+        <h2>Hope you liked our service!</h2>
+    </div>
+    <button class="btn3" onclick="window.location.href='index.php'">Go back to Home</button>
+    <?php include('partials/footer.php'); ?>
+</body>
+</html>
